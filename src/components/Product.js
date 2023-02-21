0b5230ab2cddd8a3f982903a6cbfa9ff86115227
Elif Yildiz <elifyca@gmail.com>
2023-02-21 10:51:03 -0800

    added

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,31 @@ import {useState } from 'react'
 function Product ({ product }) {
     return(
         <>
-<h3>{product.title}</h3>
+       <div className="product">
+         <h6>{product.title}</h6>
+
+         <div className='price'>${product.price}</div>
+
+         <div className='actions'></div>
+          <button>Sell</button>
+         <span className='amount'> 0 </span>
+          <button>Buy</button>
+          
+         <style jsx>{`
+       .product{
+        padding: 10px;
+        background: #fff;
+        border: 1px solid #ddd;
+        margin-bottom: 20px;
+
+       }
+
+       `}</style>
+         
+         
+         
+       
+</div>
         </>
     )
 }
