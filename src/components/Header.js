9e8d9237ff19c9d70f0e87627421cb407484514f
Elif Yildiz <elifyca@gmail.com>
2023-02-21 10:05:28 -0800

    added

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function Header ({money}){
+    return(
+        <>
+        <div>
+          You have {money}$100 to spend!
+        </div>
+        </>
+    )
+    
+
+}
